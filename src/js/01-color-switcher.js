111f03c8fdf086ebb4b68398fb07b0b759a28bf9
Alekstitula84 <alekstitula84@gmail.com>
2023-02-04 19:57:13 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
 
+const stopBtn = document.querySelector('button[data-stop]');
+
+const bodyStyle = document.querySelector("body");
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        changecolour();
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+
+});
+
+
+function changecolour() {
+    bodyStyle.style.backgroundColor = getRandomHexColor();
+}
